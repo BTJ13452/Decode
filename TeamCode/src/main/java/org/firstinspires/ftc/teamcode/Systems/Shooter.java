6c3caac1df5e9ce204af3b8aea6aa0aea617c504
BTJ-Studio <ftc.btj@gmail.com>
2026-02-22 16:17:58 +0200

    shooter speeds by volts better

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Systems/Shooter.java
@@ -13,8 +13,8 @@ public class Shooter {
     public final int MID_CELL_IN_POWERS = 1;
     public final int FAR_CELL_IN_POWERS = 2;
     public final double[][] SHOOTER_POWERS = {
-            {0.65, 0.65, 0.65, 0.65},   // CLOSE
-            {0.83, 0.78, 0.73, 0.7},   // MID
+            {0.74, 0.69, 0.65, 0.61},   // CLOSE
+            {0.83, 0.78, 0.74, 0.7},   // MID
             {0.940, 0.890, 0.860, 0.830}    // FAR
     };
 
