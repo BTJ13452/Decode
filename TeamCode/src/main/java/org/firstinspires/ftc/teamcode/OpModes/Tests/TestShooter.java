768b5fb517dd661973cea86402c5a2eac2207cef
BTJ-Studio <ftc.btj@gmail.com>
2026-03-22 19:07:58 +0200

    shoter pidf fix (more acuret pidf)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/TestShooter.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/Tests/TestShooter.java
@@ -4,12 +4,11 @@ package org.firstinspires.ftc.teamcode.OpModes.Tests;
 import static android.os.SystemClock.sleep;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.Limelight3A;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.PIDFCoefficients;
 import com.qualcomm.robotcore.hardware.VoltageSensor;
 
@@ -20,98 +19,87 @@ import org.firstinspires.ftc.teamcode.Systems.Shooter;
 @TeleOp
 @Config
 public class TestShooter extends OpMode {
-    public DcMotorEx shooterMotor;
-    double curTargetVelocity = velocity;
 
-
-//    double F = 0;
-//    double P = 300;
-
-    double[] stepSizes = {10.0, 1.0, 0.1, 0.01, 0.0001};
-
-    int stepIndex = 1;
-
-
-//    Shooter shooter;
-//    Intake intake;
-//    Limelight3A limelight;
-//    VoltageSensor voltageSensor;
-
-
-    public static double velocity = 1000;
-    public static double error = 70;
-    public static double kfError = 10;
-    public static double kp = 0.00001;
-    public static double kd = 0.0001;
-    public static double kf = 700;
-    public static long timeBetweenUpdates = 20;
+    public double targetVelocity;
+    public double preTargetVelocity;
+    public double error = 70;
+    public double kfError = 10;
+    public double kp = 0.00001;
+    public double kd = 0.0001;
+    public double kf = 400;
+    boolean targetNull = true;
 
 
     Shooter shooter;
     Intake intake;
     VoltageSensor voltageSensor;
+    LLResult llResult;
+    Limelight3A limelight;
 
 
     @Override
     public void init() {
 
         shooter = new Shooter(hardwareMap);
-        telemetry.addLine("Int complete");
-
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(kp, 0, kd, kf);
-        shooter.shooterMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+//        telemetry.addLine("Int complete");
 
         intake = new Intake(hardwareMap);
         intake.startAll(Intake.Direction.FORWARD);
-//
+        shooter.runByPidf();
+
+
         voltageSensor = hardwareMap.voltageSensor.iterator().next();
-//
-//
-//        limelight = hardwareMap.get(Limelight3A.class, "limelight");
-//        telemetry.setMsTransmissionInterval(11);
-//
-//        limelight.pipelineSwitch(0);
-//        limelight.start();
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        telemetry.setMsTransmissionInterval(11);
+
+        limelight.pipelineSwitch(1);
+        limelight.start();
 
 
     }
 
     @Override
     public void loop() {
-        if (shooter.getVelocity() >= velocity + error){
-            shooter.setVelocity(kfError);
-            sleep( timeBetweenUpdates);
-        } else shooter.setVelocity(velocity);
+        if (llResult != null) {
+            llResult = limelight.getLatestResult();
+            double ta = llResult.getTa();
+////            targetVelocity = (shooter.ShooterByDistance(ta));
+        }
+        shooter.setVelocity(targetVelocity);
 
-        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(kp, 0, kd, kf);
-        shooter.shooterMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        shooter.runByPidf();
 
-
-        double curVelocity = shooter.getVelocity();
-        double error = curTargetVelocity - curVelocity;
-
+//        if (shooter.getVelocity() >= targetVelocity + error) {
+//            shooter.setVelocity(kfError);
+//            telemetry.addLine("shooter stabilisation");
+//
+//        } else targetNull = true;
 
         if (gamepad1.dpadUpWasPressed()) {
-            velocity += 50;
+            targetVelocity += 50;
         }
 
         if (gamepad1.dpadRightWasPressed()) {
-            velocity += 50;
+            targetVelocity += 25;
         }
 
         if (gamepad1.dpadDownWasPressed()) {
-            velocity -= 50;
+            targetVelocity -= 50;
         }
 
         if (gamepad1.dpadLeftWasPressed()) {
-            velocity -= 50;
+            targetVelocity -= 25;
         }
 
-        telemetry.addData("VOLTAGE", voltageSensor.getVoltage());
         telemetry.addData("Velocity", shooter.getVelocity());
-        telemetry.addData("Target Velocity", velocity);
-        telemetry.update();
+        telemetry.addData("Target Velocity", targetVelocity);
+        telemetry.addData("null", llResult == null);
+        if (llResult != null) {
+            telemetry.addData("ta", llResult.getTa());
 
+        }
+
+        telemetry.update();
 
     }
 
@@ -120,6 +108,5 @@ public class TestShooter extends OpMode {
     public void stop() {
         shooter.setVelocity(0);
     }
-
-
 }
+
